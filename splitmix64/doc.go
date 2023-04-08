575ee0d5306d5891170496979bfcd186bbcb22a2
Kevin Mazzarelli <kevin@localhost-live.attlocal.net>
2023-04-08 17:25:46 -0500

    Add reference

--- splitmix64/doc.go
+++ splitmix64/doc.go
@@ -1,2 +1,2 @@
-// Package splitmix64 implements a pseudorandom number generator using the SplitMix64 algorithm. See [add paper later]
+// Package splitmix64 implements a pseudorandom number generator using the SplitMix64 algorithm. See ![Steele et al.](https://stochastics.ai/Steele_2014.pdf) for details.
 package splitmix64

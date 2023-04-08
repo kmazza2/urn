ede15ba7b586bacb0cbacdc00e08ffb263775147
Kevin Mazzarelli <kevin@localhost-live.attlocal.net>
2023-04-08 14:29:01 -0500

    Incomplete SplitMix64 test

---
+++ splitmix64/splitmix64.go
@@ -0,0 +1,18 @@
+package splitmix64
+
+type SplitMix64 struct {
+	state uint64
+}
+
+func NewSplitMix64(seed uint64) SplitMix64 {
+	var rng SplitMix64 = SplitMix64{seed}
+	return rng
+}
+
+func (rng SplitMix64) Next() uint64 {
+	rng.state += 0x9e3779b97f4a7c15
+	var z uint64 = rng.state
+	z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9
+        z = (z ^ (z >> 27)) * 0x94d049bb133111eb
+        return z ^ (z >> 31)
+}
---
+++ splitmix64/splitmix64_test.go
@@ -0,0 +1,7 @@
+package splitmix64
+
+import "testing"
+
+func TestAgainstCReference(t *testing.T) {
+	t.Error(`Test not implemented`)
+}

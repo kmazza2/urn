73140708d4b09f5aac171bfbd59204cb0f6c345a
Kevin Mazzarelli <kevin@localhost-live.attlocal.net>
2023-04-08 15:23:08 -0500

    SplitMix64 implementation done

--- splitmix64/splitmix64.go
+++ splitmix64/splitmix64.go
@@ -9,10 +9,10 @@ func NewSplitMix64(seed uint64) SplitMix64 {
 	return rng
 }
 
-func (rng SplitMix64) Next() uint64 {
+func (rng *SplitMix64) Next() uint64 {
 	rng.state += 0x9e3779b97f4a7c15
 	var z uint64 = rng.state
 	z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9
-        z = (z ^ (z >> 27)) * 0x94d049bb133111eb
-        return z ^ (z >> 31)
+	z = (z ^ (z >> 27)) * 0x94d049bb133111eb
+	return z ^ (z >> 31)
 }
--- splitmix64/splitmix64_test.go
+++ splitmix64/splitmix64_test.go
@@ -1,7 +1,54 @@
 package splitmix64
 
-import "testing"
+import (
+	"io/ioutil"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+func TestGeneratorAdvances(t *testing.T) {
+	var rng SplitMix64 = NewSplitMix64(uint64(0))
+	var first_number uint64 = rng.Next()
+	var second_number uint64 = rng.Next()
+	if first_number == second_number {
+		t.Error(`RNG did not advance`)
+	}
+}
 
 func TestAgainstCReference(t *testing.T) {
-	t.Error(`Test not implemented`)
+	var raw_ref_str_data []byte
+	var err error
+	raw_ref_str_data, err = ioutil.ReadFile(`testdata/c_impl_result`)
+	if err != nil {
+		t.Error(`Could not open file containing test data`)
+	}
+	var ref_str_data []string = strings.Split(string(raw_ref_str_data), "\n")
+	ref_str_data = ref_str_data[:len(ref_str_data)-1]
+	var ref_data []uint64 = make([]uint64, 100)
+	for i, str_num := range ref_str_data {
+		var converted_num uint64
+		var err error
+		converted_num, err = strconv.ParseUint(str_num, 10, 64)
+		if err != nil {
+			t.Error(`Could not parse data`)
+		}
+		ref_data[i] = converted_num
+	}
+	var data []uint64 = make([]uint64, 100)
+	for i := 0; i < 10; i++ {
+		var rng SplitMix64 = NewSplitMix64(uint64(i))
+		for j := 0; j < 10; j++ {
+			data[10*i+j] = rng.Next()
+		}
+	}
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+		}
+	}
+	for k := 0; k < 100; k++ {
+		if data[k] != ref_data[k] {
+			t.Error(`Generated data does not match reference data`)
+		}
+	}
 }

4c8243474798e8f6fb0af768151aa668caeba61c
Kevin Mazzarelli <kevin@localhost-live.attlocal.net>
2023-04-09 00:38:27 -0500

    Add reference for /dev/urandom

--- urandom/doc.go
+++ urandom/doc.go
@@ -1,2 +1,5 @@
 // Package urandom provides a function returning a random uint64 from the OS.
+// Since urn only supports RHEL, this is implemented by reading bytes from /dev/urandom. Information on /dev/urandom can be found [here].
+//
+// [here]: https://stochastics.ai/Edge_2022.html
 package urandom
